4dfbd56c27e41d861145e29153ede8c6480b22c8
Andrey Popp <8mayday@gmail.com>
2013-07-23 02:06:31 +0400

    more meat on GitPeer::Git

--- Gemfile
+++ Gemfile
@@ -1,2 +1,6 @@
 source 'https://rubygems.org'
+
+# before 0.12 release
+gem 'roar', git: 'https://github.com/apotonick/roar.git'
+
 gemspec
--- lib/gitpeer.rb
+++ lib/gitpeer.rb
@@ -1,4 +1,5 @@
 require 'uri_template'
+require 'representable/json/hash'
 require 'roar/representer/json'
 require 'roar/representer/feature/hypermedia'
 require 'rugged'
@@ -8,28 +9,7 @@ require 'gitpeer/controller'
 
 module GitPeer
 
-  class Git < Controller
-    uri :branch,    '/branch/{id}'
-    uri :tag,       '/tag/{id}'
-    uri :commit,    '/commit/{id}'
-    uri :tree,      '/tree/{id}'
-    uri :blob,      '/blob/{id}'
-    uri :object,    '/{id}'
-
-    get '/' do
-      'git'
-    end
-    get :branch
-    get :tag
-    get :commit do
-      commit = or_404 { repo.lookup(captures[:id]) }
-      not_found unless commit.type == :commit
-      commit.extend(CommitRepresenter).to_json
-    end
-    get :tree
-    get :blob
-    get :object
-
+  module GitController
     def or_404
       result = begin
         yield
@@ -43,14 +23,86 @@ module GitPeer
       not_found if result == nil
       result
     end
+  end
+
+  module JSONController
+    def json(obj, with: nil)
+      if with
+        _self = self
+        helpers = Module.new do
+          define_method :uri do |name, **vars|
+            _self.uri(name, **vars)
+          end
+        end
+        obj.extend(with).extend(helpers).to_json
+      else
+        obj.to_json
+      end
+    end
+  end
+
+  class Git < Controller
+    include GitController
+    include JSONController
+
+    uri :branch,    '/branch/{id}'
+    uri :tag,       '/tag/{id}'
+    uri :commit,    '/commit/{id}'
+    uri :commit,    '/commit/{id}'
+    uri :tree,      '/tree/{id}'
+    uri :blob,      '/blob/{id}'
+    uri :object,    '/{id}'
+
+    [:branch, :tag, :commit, :tree, :blob, :object].each do |type|
+      get type do
+        obj = or_404 { repo.lookup(captures[:id]) }
+        not_found unless type == :object or obj.type == type
+        json obj, with: representer_for(obj.type)
+      end
+    end
 
     module CommitRepresenter
       include Roar::Representer::JSON
       include Roar::Representer::Feature::Hypermedia
 
+      property :oid, as: :id
       property :message
       property :author
+      property :committer
+      property :tree_id
+      link :self do uri :commit, id: oid end
+      link :tree do uri :tree, id: tree_id end
     end
+
+    module TreeEntryRepresenter
+      include Representable::JSON::Hash
+      include Roar::Representer::Feature::Hypermedia
+    end
+
+    module TreeRepresenter
+      include Roar::Representer::JSON
+      include Roar::Representer::Feature::Hypermedia
+
+      property :oid, as: :id
+      collection :entries, extend: TreeEntryRepresenter
+      link :self do
+        uri :tree, id: oid
+      end
+    end
+
+    protected
+
+      def representer_for(type)
+        case type
+        when :commit
+          CommitRepresenter
+        when :tree
+          TreeRepresenter
+        else
+          raise "don't know how to represent #{type}"
+        end
+      end
+
   end
 
   class Code < Controller

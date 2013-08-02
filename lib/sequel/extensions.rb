846593dbff8de70bf0d53a3546fb78c7fa92f226
Andrey Popp <8mayday@gmail.com>
2013-08-02 22:28:33 +0400

    sequel: Dataset#insertFrom for inserting from Structs

--- lib/sequel/extensions.rb
+++ lib/sequel/extensions.rb
@@ -1,12 +1,18 @@
 module Sequel
 
   class Dataset
+
+    def insertFrom(data)
+      insert data.to_h.select { |k, v| columns.include? k }
+    end
+
     def as(cls)
       to_a.map do |row|
         values = cls.members.map { |k| row[k] }
         cls.new(*values)
       end
     end
+
   end
 end
 
